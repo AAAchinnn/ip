28f3b3ba85dc3fbb44153075b6ea56c40118cb27
Achinthieya <Achin@Srinivasans-MacBook-Pro.local>
2026-08-17 16:02:39 +0800

    Level-0  Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/Jeremy.java
@@ -0,0 +1,7 @@
+public class Jeremy {
+    public static void main(String[] args) {
+
+        System.out.println("Hello!, I'm Jeremy\nWhat can I do for you?");
+	System.out.println("Hits you with a surprise left");
+    }
+}
